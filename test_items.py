3418f08960c9cb73cd7e435425c393035696ba24
DaryaVbr <dar.xunxun@gmail.com>
2025-04-30 12:27:42 +0300

    небольшой апдейт по ассерту для нахождения кнопки добавления товара

--- test_items.py
+++ test_items.py
@@ -10,6 +10,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_presence_add_button(browser):
     browser.get(link)
     time.sleep(30)
-    # add_but = browser.find_element(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
 
     assert browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket"), "add button not found"
